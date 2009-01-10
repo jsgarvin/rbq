3653e04298f8c769da4b3e0a60149630369c7dda
Jonathan Garvin <jon@5valleys.com>
2009-01-09 22:46:18 -0700

    playlist now singleton

--- lib/manager.rb
+++ lib/manager.rb
@@ -6,19 +6,19 @@ module RBQ
     
       def set_path(path)
         Library.setup(path)
-        @playlist = Playlist.new(path)
+        Playlist.setup(path)
       end
       
       def build_new_queue
-        playlist.clear
-        while playlist.hours < 8 and Library.total_weight > 0
+        Playlist.clear
+        while Playlist.hours < 8 and Library.total_weight > 0
           new_song = Library.pick_a_song
-          unless playlist.songs.include?(new_song)
-            playlist.add_songs(new_song)
+          unless Playlist.songs.include?(new_song)
+            Playlist.add_songs(new_song)
           end
         end
-        playlist.spread(25)
-        playlist.save
+        Playlist.spread(25)
+        Playlist.save
       end
     end
   end
