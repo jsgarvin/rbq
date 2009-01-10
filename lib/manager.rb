7bbb870760b8203f44324ad0a0e0a86a4428bea3
Jonathan Garvin <jon@5valleys.com>
2009-01-09 22:17:15 -0700

    library now a singleton

--- lib/manager.rb
+++ lib/manager.rb
@@ -2,17 +2,17 @@ module RBQ
   class Manager
     include Singleton
     class << self
-      attr_reader :library, :playlist
+      attr_reader :playlist
     
       def set_path(path)
-        @library = Library.new(path)
+        Library.setup(path)
         @playlist = Playlist.new(path)
       end
       
       def build_new_queue
         playlist.clear
-        while playlist.hours < 8 and library.total_weight > 0
-          new_song = library.pick_a_song
+        while playlist.hours < 8 and Library.total_weight > 0
+          new_song = Library.pick_a_song
           unless playlist.songs.include?(new_song)
             playlist.add_songs(new_song)
           end
