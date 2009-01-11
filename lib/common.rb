e304818d5cf58bd0cc87d59c19f9b42d8394718b
Jonathan Garvin <jon@5valleys.com>
2009-01-11 13:17:55 -0700

    add common.rb

---
+++ lib/common.rb
@@ -0,0 +1,29 @@
+module RBQ
+  class XmlFileNotFoundError < RuntimeError; end
+
+  module SharedLibraryPlaylistMethods
+    include REXML
+    
+    def backup_xml_file
+      File.copy(expanded_path_to_file, expanded_path_to_file+".#{Time.now.to_i.to_s}.bkp")
+    end
+    
+    def xml_file_exists?
+      File.exists?(expanded_path_to_file)
+    end
+    
+    def xml_doc
+      raise XmlFileNotFoundError unless @xml_doc or xml_file_exists?
+      @xml_doc ||= Document.new(File.new(expanded_path_to_file))
+    end
+    
+    #######
+    private
+    #######
+    
+    def expanded_path_to_file
+      @expanded_path_to_file ||= File.expand_path("#{path}/#{filename}")
+    end
+    
+  end
+end
